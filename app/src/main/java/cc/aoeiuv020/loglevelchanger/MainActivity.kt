ba2e1ab68ce6b22f702a67dbc7d54b5a5f99f332
AoEiuV020 <aoeiuv020@gmail.com>
2019-02-01 13:19:09 +0800

    新建项目

---
+++ app/src/main/java/cc/aoeiuv020/loglevelchanger/MainActivity.kt
@@ -0,0 +1,12 @@
+package cc.aoeiuv020.loglevelchanger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
